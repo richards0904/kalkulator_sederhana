fe4df6bc979acc5872df81b24c2439a29f642ab0
Richard Satya <113106414+richards0904@users.noreply.github.com>
2023-10-10 16:44:26 +0700

    Initial commit

---
+++ bin/kalkulator_sederhana.dart
@@ -0,0 +1,5 @@
+import 'package:kalkulator_sederhana/kalkulator_sederhana.dart' as kalkulator_sederhana;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kalkulator_sederhana.calculate()}!');
+}
---
+++ test/kalkulator_sederhana_test.dart
@@ -0,0 +1,8 @@
+import 'package:kalkulator_sederhana/kalkulator_sederhana.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
